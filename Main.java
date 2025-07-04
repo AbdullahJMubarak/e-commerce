e24ba4332448c81facb6a2f5bb89df0c3b11c1b4
Abdullah Mubarak <abdalluh.moubarak@gmail.com>
2025-07-04 23:05:35 +0300

    Add test cases to main file

--- Main.java
+++ Main.java
@@ -2,16 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Shopper customer = new Shopper("Abdalla", 1000);
 
-        // Add items to cart
-        // Cart cart = new Cart();
-        // cart.add(cheese, 2);
-        // cart.add(biscuits, 1);
-        // cart.add(tv, 1);
-        // cart.add(scratchCard, 1);
+        Item cheese = new PerishableItem("Cheese", 100, 5,
+                new GregorianCalendar(2025, Calendar.DECEMBER, 1).getTime(), 0.2);
+        Item biscuits = new PerishableItem("Biscuits", 150, 3,
+                new GregorianCalendar(2025, Calendar.DECEMBER, 1).getTime(), 0.7);
+        Item tv = new DurableItem("TV", 300, 4, 3.0);
+        Item scratchCard = new VirtualItem("Mobile Scratch Card", 50, 10);
 
-        // Process checkout
-        // CheckoutService.checkout(customer, cart);
+        ShoppingBasket cart = new ShoppingBasket();
+        cart.add(cheese, 2);
+        cart.add(biscuits, 1);
+        cart.add(tv, 1);
+        cart.add(scratchCard, 1);
 
+        PaymentProcessor.checkout(customer, cart);
     }
 }
